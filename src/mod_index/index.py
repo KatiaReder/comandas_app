650bf79e169fcb2bd3eab8385f1099a4d3bbd0d6
Katia <katireder5@gmail.com>
2023-09-26 20:40:23 -0300

    Aula 10 - 26/09

---
+++ src/mod_index/index.py
@@ -0,0 +1,6 @@
+from flask import Blueprint, render_template
+bp_index = Blueprint('index', __name__, template_folder='templates')
+
+@bp_index.route('/')
+def formIndex():
+  return render_template('formIndex.html'), 200
