6d499f4f404c4187a68ca95e54ce3f313b397981
Katia <katireder5@gmail.com>
2023-10-04 00:42:26 -0300

    Entrega 03/10

--- src/mod_funcionario/funcionario.py
+++ src/mod_funcionario/funcionario.py
@@ -2,6 +2,10 @@ from flask import Blueprint, render_template
 bp_funcionario = Blueprint('funcionario', __name__, url_prefix="/funcionario", template_folder='templates')
 
 
-@bp_funcionario.route('/')
+@bp_funcionario.route('/', methods=['GET', 'POST'])
 def formListaFuncionario():
   return render_template('formListaFuncionario.html'), 200
+
+@bp_funcionario.route('/form-funcionario', methods=['POST'])
+def formFuncionario():
+  return render_template('formFuncionario.html'), 200
--- src/mod_produto/produto.py
+++ src/mod_produto/produto.py
@@ -1,6 +1,10 @@
 from flask import Blueprint, render_template
 bp_produto = Blueprint('produto', __name__, url_prefix="/produto", template_folder='templates')
 
-@bp_produto.route('/')
+@bp_produto.route('/', methods=['GET', 'POST'])
 def formListaProduto():
   return render_template('formListaProduto.html'), 200
+
+@bp_produto.route('/form-produto/', methods=['POST'])
+def formProduto():
+  return render_template('formProduto.html'), 200
