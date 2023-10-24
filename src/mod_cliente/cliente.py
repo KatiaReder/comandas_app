e6a2b726de58bf57841eee85b702400d0ef89f05
Katia <katireder5@gmail.com>
2023-10-24 16:52:51 -0300

    Funcionalidades de GET e POST

--- src/mod_cliente/cliente.py
+++ src/mod_cliente/cliente.py
@@ -1,10 +1,46 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
+import requests
+from settings import HEADERS_API, ENDPOINT_CLIENTE
+from funcoes import Funcoes
 bp_cliente = Blueprint('cliente', __name__, url_prefix="/cliente", template_folder='templates')
 
 @bp_cliente.route('/', methods=['GET', 'POST'])
 def formListaCliente():
-  return render_template('formListaCliente.html'), 200
+  try:
+    response = requests.get(ENDPOINT_CLIENTE, headers=HEADERS_API)
+    result = response.json()
+    if (response.status_code != 200):
+      raise Exception(result[0])
+
+    return render_template('formListaCliente.html', result=result[0])
+  except Exception as e:
+    return render_template('formListaCliente.html', msgErro=e.args[0])
 
 @bp_cliente.route('/form-cliente/', methods=['POST'])
 def formCliente():
-  return render_template('formCliente.html'), 200
+  return render_template('formCliente.html')
+
+@bp_cliente.route('/insert', methods=['POST'])
+def insert(): 
+  try:
+    id_cliente = request.form['id']
+    nome = request.form['nome']
+    cpf = request.form['cpf']
+    telefone = request.form['telefone']
+    dia_fiado = request.form['dia_fiado']
+    compra_fiado = request.form['compra_fiado']
+    senha = Funcoes.cifraSenha(request.form['senha'])
+
+    payload = {'id_cliente': id_cliente, 'nome': nome, 'cpf': cpf, 'telefone': telefone, 'dia_fiado': dia_fiado, 'compra_fiado': compra_fiado, 'senha': senha}
+
+    response = requests.post(ENDPOINT_CLIENTE, headers=HEADERS_API, json=payload)
+    result = response.json()
+    print(result)
+    print(response.status_code)
+
+    if (response.status_code != 200 or result[1] != 200):
+      raise Exception(result[0])
+
+    return redirect(url_for('cliente.formListaCliente', msg=result[0]))
+  except Exception as e:
+    return render_template('formListaCliente.html', msgErro=e.args[0])
--- src/mod_funcionario/funcionario.py
+++ src/mod_funcionario/funcionario.py
@@ -1,11 +1,48 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
+import requests
+from settings import HEADERS_API, ENDPOINT_FUNCIONARIO
+from funcoes import Funcoes
 bp_funcionario = Blueprint('funcionario', __name__, url_prefix="/funcionario", template_folder='templates')
 
-
+''' rotas dos formulários '''
 @bp_funcionario.route('/', methods=['GET', 'POST'])
 def formListaFuncionario():
-  return render_template('formListaFuncionario.html'), 200
+    try:
+      response = requests.get(ENDPOINT_FUNCIONARIO, headers=HEADERS_API)
+      result = response.json()
+      if (response.status_code != 200):
+          raise Exception(result[0])
 
-@bp_funcionario.route('/form-funcionario', methods=['POST'])
+      return render_template('formListaFuncionario.html', result=result[0])
+    except Exception as e:
+      return render_template('formListaFuncionario.html', msgErro=e.args[0])
+
+@bp_funcionario.route('/form-funcionario/', methods=['POST'])
 def formFuncionario():
-  return render_template('formFuncionario.html'), 200
+    return render_template('formFuncionario.html')
+
+@bp_funcionario.route('/insert', methods=['POST'])
+def insert():
+    try:
+
+      id_funcionario = request.form['id']
+      nome = request.form['nome']
+      matricula = request.form['matricula']
+      cpf = request.form['cpf']
+      telefone = request.form['telefone']
+      grupo = request.form['grupo']
+      senha = Funcoes.cifraSenha(request.form['senha'])
+
+      payload = {'id_funcionario': id_funcionario, 'nome': nome, 'matricula': matricula, 'cpf': cpf, 'telefone': telefone, 'grupo': grupo, 'senha': senha}
+
+      response = requests.post(ENDPOINT_FUNCIONARIO, headers=HEADERS_API, json=payload)
+      result = response.json()
+      print(result) 
+      print(response.status_code) 
+
+      if (response.status_code != 200 or result[1] != 200):
+        raise Exception(result[0])
+
+      return redirect(url_for('funcionario.formListaFuncionario', msg=result[0]))
+    except Exception as e:
+      return render_template('formListaFuncionario.html', msgErro=e.args[0])
